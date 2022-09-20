f7f6b69ba5c5d0c9841cebb582548365689ea6df
Nailton2002 <jnmp25jnmp@gmail.com>
2022-09-20 17:25:49 -0300

    Consulta com query methods

--- src/main/java/br/com/workshopmongo/config/Instantiation.java
+++ src/main/java/br/com/workshopmongo/config/Instantiation.java
@@ -7,7 +7,6 @@ import java.util.TimeZone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.repository.CrudRepository;
 
 import br.com.workshopmongo.domain.Post;
 import br.com.workshopmongo.domain.User;
@@ -43,7 +42,7 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(nailton));
+		CommentDTO c1 = new CommentDTO("Boa viagem filha!", sdf.parse("21/03/2018"), new AuthorDTO(nailton));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(mercia));
 		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(nailton));
 		
